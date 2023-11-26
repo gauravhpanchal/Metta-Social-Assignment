3f0c52d3a2a76138eccbafc90c5a63a67cc67205
Gaurav <gauravhpanchal2@gmail.com>
2023-11-26 11:45:06 +0530

    Loading Spinner added and added all countries card on 1st load

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,66 @@
-import React, { useState } from "react";
-import { SEARCH_BY_CURRENCY } from "../utils/constant";
+import React, { useEffect, useState } from "react";
+import { ALL_COUNTRIES, SEARCH_BY_CURRENCY } from "../utils/constant";
 import CountryCard from "./CountryCard";
+import { ColorRing } from "react-loader-spinner";
+
+/**
+ * Main component for exploring countries by currency.
+ *
+ * This component provides a search functionality to explore countries based on
+ * a specific currency. It fetches all countries on initial load and allows users
+ * to search for countries by entering a currency code. It displays a loading
+ * spinner while fetching data and handles errors in case of failed requests.
+ */
 
 const Main = () => {
-  const [currency, setCurrency] = useState("");
-  const [countries, setCountries] = useState([]);
-  const [error, setError] = useState(null);
+  const [currency, setCurrency] = useState(""); // Input value for currency search
+  const [countries, setCountries] = useState([]); // List of countries based on search
+  const [allCountries, setAllCountries] = useState([]); // List of all countries
+  const [error, setError] = useState(null); //Error message if data fetching fails
+  const [loading, setLoading] = useState(true); // Loading state during data fetching
 
-  //   console.log(currency);
+  // Fetch all countries on component mount
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
+  /**
+   * Fetches data for all countries and sets the state variables accordingly.
+   */
+  const fetchAllCountries = async () => {
+    setLoading(true);
+    const allCountriesRes = await fetch(ALL_COUNTRIES);
+    const allCountriesData = await allCountriesRes.json();
+    setAllCountries(allCountriesData);
+    setLoading(false);
+  };
+
+  /**
+   * Handles the currency search functionality.
+   */
   const handleCurrency = async () => {
     try {
       if (!currency) return;
+      setLoading(true); // Set loading to true before making the request
       const currRes = await fetch(SEARCH_BY_CURRENCY + currency);
       const currData = await currRes.json();
+      setAllCountries([]); // Clear all countries when searching by currency
       setCountries(currData);
       setError(null);
+      setCurrency(""); // Reset the currency input after successful search
     } catch (err) {
       console.error("Fetching data failed:", err.message);
       setCountries([]);
       setError("Error fetching data. Please try again.");
+    } finally {
+      setLoading(false); // Set loading to false when the request is complete (success or error)
     }
-
-    // console.log(currData);
   };
-  console.log(countries);
   return (
     <div className="text-center items-center">
-      <p className="text-3xl font-bold">Explore Countries by Currency</p>
+      <p className="text-4xl font-bold">Explore Countries by Currency</p>
       {/* Search Bar */}
-      <div className="mt-20 sm-flex flex-wrap text-center">
+      <div className="mt-10 sm-flex flex-wrap text-center">
         <input
           type="text"
           value={currency}
@@ -46,7 +78,38 @@ const Main = () => {
       </div>
       {/* Error Handling */}
       {error && <p className="mt-4 text-red-400">{error}</p>}
-      {/* Countries Cards */}
+
+      {/* All Countries Card on first load */}
+
+      {
+        allCountries.length > 0 ? (
+          <>
+            <p className="font-bold text-3xl mt-4 underline">All Countries</p>
+            <div className="flex gap-10 flex-wrap justify-center mb-10">
+              {allCountries?.map((country) => (
+                <CountryCard key={country.capital} countryData={country} />
+              ))}
+            </div>
+          </>
+        ) : null /* Don't render anything here if there are no countries yet */
+      }
+
+      {/* Loading Spinner */}
+      {loading && (
+        <div className="fixed top-80 left-[550px] translate-x-2/4 translate-y-2/4 ">
+          <ColorRing
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+      )}
+      {/* Countries Cards After Search */}
+
       <div className="flex gap-16 flex-wrap justify-center mb-10">
         {Array.isArray(countries) ? (
           countries?.map((country) => (
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,7 @@
 export const SEARCH_BY_CURRENCY = "https://restcountries.com/v3.1/currency/";
 
+export const ALL_COUNTRIES = "https://restcountries.com/v3.1/all";
+
 export const FLAG_IMAGE = "https://flagsapi.com/BE/flat/64.png";
 
 export const FLAG_CODE = "https://flagcdn.com/en/codes.json";
